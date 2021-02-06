685ad9a57e87757d65a2c3e51d8f5972cdd791a7
mdjahiulislam <jahidj2255@gmail.com>
2021-02-06 17:52:02 +0600

    this is thired assignmet

---
+++ assignment.js
@@ -0,0 +1,39 @@
+ function kilometerToMeter (num){
+     if (num < 0){
+         return "Input value is not Correct";
+    }
+     else{
+       return num*1000;
+     }
+ }
+
+function budgetCalculator(watch, mobile, laptop){
+  var watchPrice = watch*50;
+  var mobilePrice = mobile*100;
+  var laptopPrice = laptop*500;
+  return watchPrice + mobilePrice + laptopPrice;
+
+}
+
+
+function hotelCost(day){
+  if(day == 0 || day < 0){
+    return "--ERROR-- plese give right value";
+  }
+  else if(day <=10){
+    return day*100;
+  }
+  else if(day <= 20 ){
+    var fristStep = 10*100;
+    var remaining = day-10;
+    var secondStep = remaining*80;
+    return fristStep+secondStep;
+  }
+  else {
+    var fristStep = 10*100;
+    var secondStep = 10*80
+    var remaining = day-20;
+    var thiredStep =  remaining*50;
+    return fristStep+secondStep+thiredStep;
+  }
+}
